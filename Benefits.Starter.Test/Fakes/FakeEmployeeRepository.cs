a610ab65c3b745ef85bed964cb73e3c2221d6877
Allegra Bourne <185409591+allegrabourne@users.noreply.github.com>
2026-03-03 20:23:25 +0000

    Added employee service with associated business logic and unit tests, created fake repository rather than a moq (could use either), added missing test project packages

---
+++ Benefits.Starter.Test/Fakes/FakeEmployeeRepository.cs
@@ -0,0 +1,29 @@
+﻿using Benefits.Starter.Domain;
+using Benefits.Starter.Service.Employees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benefits.Starter.Test.Fakes
+{
+    public sealed class FakeEmployeeRepository : IEmployeeRepository
+    {
+        public bool EmailExists { get; set; }
+        public bool EmployeeNoExists { get; set; }
+        public Employee? AddedEmployee { get; private set; }
+        public int AddCallCount { get; private set; }
+
+        public Task AddAsync(Employee employee, CancellationToken cancellationToken)
+        {
+            AddedEmployee = employee;
+            AddCallCount++;
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> EmailExistsAsync(string email, CancellationToken cancellationToken)
+            => Task.FromResult(EmailExists);
+
+        public Task<bool> EmployeeNoExistsAsync(string employeeNo, CancellationToken cancellationToken)
+            => Task.FromResult(EmployeeNoExists);
+    }
+}
